8cf9394465748136c90e7ef7114a994e95fd4264
RobertoJavaDev <robertojavadev@gmail.com>
2023-02-26 15:04:09 +0100

    Implemented addBlock method

--- src/main/java/pl/robertojavadev/Wall.java
+++ src/main/java/pl/robertojavadev/Wall.java
@@ -1,12 +1,17 @@
 package pl.robertojavadev;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public class Wall implements Structure {
 
-    private List<Block> blocks;
+    private List<Block> blocks = new ArrayList<>();
+
+    public void addBlock(Block block) {
+        blocks.add(block);
+    }
 
     @Override
     public Optional<Block> findBlockByColor(String color) {
