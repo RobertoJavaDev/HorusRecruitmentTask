67801876b5c4a7bec84a1997679f4d4157b3f33c
RobertoJavaDev <robertojavadev@gmail.com>
2023-02-26 15:04:44 +0100

    Added BlockImpl class with implementation

---
+++ src/main/java/pl/robertojavadev/BlockImpl.java
@@ -0,0 +1,22 @@
+package pl.robertojavadev;
+
+public class BlockImpl implements Block {
+
+    private final String color;
+    private final String material;
+
+    public BlockImpl(String color, String material) {
+        this.color = color;
+        this.material = material;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String getMaterial() {
+        return material;
+    }
+}
